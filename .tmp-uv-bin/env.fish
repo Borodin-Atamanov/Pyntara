9fb541ec9dfcddfee87db888ae0df6e6f6a3bf49
Borodin-Atamanov <argentidin@gmail.com>
2026-08-03 01:28:42 -0300

    chore: remove legacy installer scripts and fix repo anomalies - Delete i.sh, install.sh, diag_pipe.sh (legacy bootstrap scripts) - Delete .tmp-uv-bin/ (accidentally committed uv binaries), ignore it in .gitignore - Commit uv.lock sync (version 0.1.1, pytest-timeout)

--- .tmp-uv-bin/env.fish
+++
@@ -1,4 +0,0 @@
-if not contains "$HOME/Downloads/Pyntara/.tmp-uv-bin" $PATH
-    # Prepending path in case a system-installed binary needs to be overridden
-    set -x PATH "$HOME/Downloads/Pyntara/.tmp-uv-bin" $PATH
-end
